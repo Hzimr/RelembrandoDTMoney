99c69487bd4265bf8f8a4aaec6cd2b2f743e654b
Hítalo Rodrigues <hitalo005@gmail.com>
2024-05-14 15:08:09 -0300

    feat: created TransactionTypeButton

--- src/components/newTransactionModal/newTransactionModal.tsx
+++ src/components/newTransactionModal/newTransactionModal.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { CloseButton, Content, Overlay } from './newTransactionModalStyles'
-import { X } from '@phosphor-icons/react'
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from './newTransactionModalStyles'
+import { ArrowCircleDown, ArrowCircleUp, X } from '@phosphor-icons/react'
 
 export function NewTransactionModal() {
   return (
@@ -15,6 +21,17 @@ export function NewTransactionModal() {
           <input type="text" placeholder="Descrição" required></input>
           <input type="number" placeholder="Preço" required></input>
           <input type="text" placeholder="Categoria" required></input>
+          <TransactionType>
+            <TransactionTypeButton variant="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
+
           <button type="submit">Cadastrar</button>
         </form>
       </Content>
