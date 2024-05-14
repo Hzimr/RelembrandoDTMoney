e2dcc52af0275c7c4da6b33d0dd952bbf3ea2ff1
Hítalo Rodrigues <hitalo005@gmail.com>
2024-05-14 10:17:29 -0300

    feat: created component header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,18 @@
+import {
+  HeaderContainer,
+  HeaderContent,
+  NewTransactionButton,
+} from './headerStyles'
+
+import logoImg from '../../assets/logo.svg'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+        <NewTransactionButton>Nova transação</NewTransactionButton>
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
