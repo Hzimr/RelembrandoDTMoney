810ed4f3d65e484e78d216c5bbcefefd6f292d5d
Hítalo Rodrigues <hitalo005@gmail.com>
2024-05-14 10:50:29 -0300

    feat: created summary component

--- src/pages/transactions/transactions.tsx
+++ src/pages/transactions/transactions.tsx
@@ -1,9 +1,11 @@
 import { Header } from '../../components/header/header'
+import { Summary } from '../../components/summary/summary'
 
 export function Transactions() {
   return (
     <div>
       <Header />
+      <Summary />
     </div>
   )
 }
