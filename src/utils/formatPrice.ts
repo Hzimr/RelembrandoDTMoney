8ace8e4fabc16ec3fc20c6b6ca059642880d0757
Hítalo Rodrigues <hitalo005@gmail.com>
2024-05-20 11:27:05 -0300

    feat: other possibility in formatPrice

--- src/utils/formatPrice.ts
+++ src/utils/formatPrice.ts
@@ -5,3 +5,10 @@ export function formatPrice(valueInCents: number) {
     currency: 'BRL',
   })
 }
+
+/* outra possível solução seria
+export const priceFormatter = new Intl.NumberFormat('pt-BR', {
+  style: 'currency',
+  currency: 'BRL',
+})
+*/
